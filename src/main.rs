b5aac1e5e2d0a5c3a11e7bc5d73649836d5c3916
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2023-06-30 15:47:02 +0800

    test normalize function
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ extern "C" {
     pub fn imdecode(arg0: i32, arg1: i32) -> i32;
     #[link_name = "wasmedge_opencvmini_imencode"]
     pub fn imencode(arg0: i32, arg1: i32, arg2: i32);
-    #[link_name = "wasmedge_opencvmini_imshow"]
-    pub fn imshow(arg0: i32, arg1: i32, arg2: i32);
-    #[link_name = "wasmedge_opencvmini_waitKey"]
-    pub fn waitkey(arg0: i32);
-    #[link_name = "wasmedge_opencvmini_blur"]
-    pub fn blur(arg0: i32) -> i32;
+
     #[link_name = "wasmedge_opencvmini_imwrite"]
     pub fn imwrite(arg0: i32, arg1: i32, arg2: i32);
+
+    #[link_name = "wasmedge_opencvmini_blur"]
+    pub fn blur(arg0: i32) -> i32;
+    #[link_name = "wasmedge_opencvmini_normalize"]
+    pub fn normalize(arg0: i32) -> i32;
 }
 
 use std::fs;
@@ -23,11 +23,12 @@ fn main() {
 
     unsafe {
         let src = imdecode(v.as_ptr() as i32, v.len() as i32);
-        // let output = blur(src);
+        let output = blur(src);
+        let output = normalize(output);
 
-        let mut buf: Vec<u8> = vec![];
-        buf.resize(v.len(), 0);
-        imencode(src, buf.as_ptr() as i32, buf.len() as i32);
+        let mut buf: Vec<f32> = vec![];
+        buf.resize(v.len(), 0.);
+        imencode(output, buf.as_ptr() as i32, buf.len() as i32);
 
         println!("{:?}", buf);
     }
