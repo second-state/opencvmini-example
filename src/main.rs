b04fc0a33ab3f720570d84f46508f451f2d27c0b
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2023-07-19 15:59:24 +0800

    testing model
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- src/main.rs
+++ src/main.rs
@@ -17,24 +17,35 @@ extern "C" {
 }
 
 use std::fs;
+use wasmedge_tensorflow_interface::{self};
 
 fn main() {
-    let v = fs::read("asset/35k.jpg").unwrap();
+    let v = fs::read("asset/35k.jpg").expect("failed to open image");
+
+    let mod_buf = fs::read("asset/lite-model_ssd_mobilenet_v1_1_metadata_2.tflite")
+        .expect("failed to open model");
+    // The mod_buf is a vec<u8> which contains the model data.
+    let mut session = wasmedge_tensorflow_interface::TFLiteSession::new(&mod_buf);
 
     println!("start");
 
+    let mut buf: Vec<f32> = vec![];
+    buf.resize(v.len(), 0.);
+
     unsafe {
-        let src = imdecode(v.as_ptr() as i32, v.len() as i32);
-        let output = blur(src);
-        let output = normalize(output);
-        let output = bilinear_sampling(output, 100, 100);
+        let img = imdecode(v.as_ptr() as i32, v.len() as i32);
+        let img = normalize(img);
 
-        let mut buf: Vec<f32> = vec![];
-        buf.resize(v.len(), 0.);
-        imencode(output, buf.as_ptr() as i32, buf.len() as i32);
-
-        println!("{:?}", buf);
+        // encode back to instance's buffer
+        imencode(img, buf.as_ptr() as i32, buf.len() as i32);
     }
+
+    println!("add input");
+    session
+        .add_input("normalized_input_image_tensor", &buf)
+        .run();
+    let res_vec: Vec<u8> = session.get_output("tensorflow/serving/classify");
+    println!("{:?}", res_vec);
 
     println!("done");
 }
